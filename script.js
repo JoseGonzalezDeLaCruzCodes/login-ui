eecfa53a1172ddb6d6872309ccce21b442c3e754
JoseGonzalezDeLaCruzCodes <josegonzcoding@gmail.com>
2025-09-04 17:38:42 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// This is a constructor
+
+// function UserID(username, password){
+//     this.username = username;
+//     this.password = password;
+// }
+
+//Saved inputs
+
+// function 
+
+
+// login
+function getUserInputs(){
+
+    // *Find out why .getElementById does not take .id/class  *
+    let usernameIn = document.getElementById('usernameInput').value;  
+
+    let passwordIn = document.getElementById('passwordInput').value;
+
+    
+}
+
+
+function checkInputs(){
+
+
+}
+
+
+
+// Sign up 
+function setUserInputs(){
+
+
+
+
+}
+
+
+
+
+
+
+
+// check if the user put the correct username and password
+
+
+// create a function that makes an account for the user 
+
+// create a function for forget pssword
+
